e35f7654555ca4208471f0aae53f87d3bc29fd3d
TillWurner <85416653+TillWurner@users.noreply.github.com>
2022-07-18 20:35:55 -0400

    jose

--- database/seeders/MicroSeeder.php
+++ database/seeders/MicroSeeder.php
@@ -16,45 +16,268 @@ class MicroSeeder extends Seeder
     {
        //hay 10 lineas y como son ida y vuelta serian 20 micros para 20 micreros
        //LINEAS DISPONIBLES
+       //Linea 1 IDA
        $micro = new Micro() ;
         $micro->placa='5503ABC';
         $micro->modelo='Volsvagen 2006';
         $micro->cant_asiento='20';
         $micro->num_interno='3485';
-        $micro->foto='https://qph.fs.quoracdn.net/main-thumb-236773730-200-iypcqnsrwocpbfnjjhpwctbxlxaifhrf.jpeg';
+        $micro->foto='https://hemeroteca.correodelsur.com/2013/11/27/ca_3_1_quique.jpg';
         $micro->fecha_asignacion='2022-01-01';
         $micro->fecha_baja='2022-03-01';
         $micro->disponible=1;
-        $micro->id_linea=15; 
+        $micro->id_linea=1; 
         $micro->id_user=2;
-        $micro->save(); 
-
+        $micro->save();
+        //Linea 1 VUELTA
+        $micro = new Micro() ;
+        $micro->placa='5503ABC';
+        $micro->modelo='Volsvagen 2006';
+        $micro->cant_asiento='20';
+        $micro->num_interno='3485';
+        $micro->foto='https://hemeroteca.correodelsur.com/2013/11/27/ca_3_1_quique.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=2; 
+        $micro->id_user=2;
+        $micro->save();
+        //Linea 2 IDA
         $micro = new Micro() ;
         $micro->placa='4014ANC';
         $micro->modelo='2015';
         $micro->cant_asiento='20';
         $micro->num_interno='6598';
-        $micro->foto='https://www.superprof.co/imagenes/anuncios/profesor-home-clases-frances-portugues-para-todas-las-personas-mexicanas-deseen-aprender-esos-bonitos-prodigiosos-idiomas.jpg';
+        $micro->foto='https://correodelsur.com/img/contents/images_840/2021/01/30/48d12101-7e5b-4439-be82-d7044d21a4ce.jpg';
         $micro->fecha_asignacion='2022-01-01';
         $micro->fecha_baja='2022-03-01';
         $micro->disponible=1;
-        $micro->id_linea=16; 
+        $micro->id_linea=3; 
         $micro->id_user=3;
         $micro->save(); 
-
+        //Linea 2 VUELTA
+        $micro = new Micro() ;
+        $micro->placa='4014ANC';
+        $micro->modelo='2015';
+        $micro->cant_asiento='20';
+        $micro->num_interno='6598';
+        $micro->foto='https://correodelsur.com/img/contents/images_840/2021/01/30/48d12101-7e5b-4439-be82-d7044d21a4ce.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=4; 
+        $micro->id_user=3;
+        $micro->save(); 
+        //Linea 5 IDA
         $micro = new Micro() ;
         $micro->placa='7783ABC';
         $micro->modelo='1999';
         $micro->cant_asiento='20';
-        $micro->num_interno='3485';
-        $micro->foto='https://www.somosiberoamerica.org/wp-content/uploads/2019/12/4039ba77f1b97fddf467329e81c2058c-225x225.jpg';
+        $micro->num_interno='7321';
+        $micro->foto='http://www.talcamicros.cl/wp-content/uploads/2019/12/Linea_5_Microbuses_Abate_Molina-300x164.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=5; 
+        $micro->id_user=4;
+        $micro->save();
+        //Linea 5 VUELTA
+        $micro = new Micro() ;
+        $micro->placa='7783ABC';
+        $micro->modelo='1999';
+        $micro->cant_asiento='20';
+        $micro->num_interno='7321';
+        $micro->foto='http://www.talcamicros.cl/wp-content/uploads/2019/12/Linea_5_Microbuses_Abate_Molina-300x164.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=6; 
+        $micro->id_user=4;
+        $micro->save();  
+        //Linea 8 IDA
+        $micro = new Micro() ;
+        $micro->placa='6551ALO';
+        $micro->modelo='1995';
+        $micro->cant_asiento='20';
+        $micro->num_interno='2468';
+        $micro->foto='https://www.atb.com.bo/fotos/1023853864.JPG';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=7; 
+        $micro->id_user=5;
+        $micro->save(); 
+        //Linea 8 VUELTA
+        $micro = new Micro() ;
+        $micro->placa='6551ALO';
+        $micro->modelo='1995';
+        $micro->cant_asiento='20';
+        $micro->num_interno='2468';
+        $micro->foto='https://www.atb.com.bo/fotos/1023853864.JPG';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=8; 
+        $micro->id_user=5;
+        $micro->save(); 
+        //Linea 9 IDA
+        $micro = new Micro() ;
+        $micro->placa='9687LOL';
+        $micro->modelo='2007';
+        $micro->cant_asiento='20';
+        $micro->num_interno='8764';
+        $micro->foto='https://1.bp.blogspot.com/-PJDglPuO684/XpXgXUFV1MI/AAAAAAAANM8/x57Vlu0eKigsMwzlx_2FzHkX03LCn3B_QCLcBGAsYHQ/s1600/bienal%2B%25281%2529.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=9; 
+        $micro->id_user=6;
+        $micro->save();
+        //Linea 9 VUELTA
+        $micro = new Micro() ;
+        $micro->placa='9687LOL';
+        $micro->modelo='2007';
+        $micro->cant_asiento='20';
+        $micro->num_interno='8764';
+        $micro->foto='https://1.bp.blogspot.com/-PJDglPuO684/XpXgXUFV1MI/AAAAAAAANM8/x57Vlu0eKigsMwzlx_2FzHkX03LCn3B_QCLcBGAsYHQ/s1600/bienal%2B%25281%2529.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=10; 
+        $micro->id_user=6;
+        $micro->save();
+        //Linea 10 IDA
+        $micro = new Micro() ;
+        $micro->placa='4298KOK';
+        $micro->modelo='2001';
+        $micro->cant_asiento='20';
+        $micro->num_interno='3344';
+        $micro->foto='http://cd1.eju.tv/wp-content/uploads/2018/02/5a89a36519509.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=11; 
+        $micro->id_user=7;
+        $micro->save(); 
+        //Linea 10 VUELTA
+        $micro = new Micro() ;
+        $micro->placa='4298KOK';
+        $micro->modelo='2001';
+        $micro->cant_asiento='20';
+        $micro->num_interno='3344';
+        $micro->foto='http://cd1.eju.tv/wp-content/uploads/2018/02/5a89a36519509.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=12; 
+        $micro->id_user=7;
+        $micro->save(); 
+        //Linea 11 IDA
+        $micro = new Micro() ;
+        $micro->placa='7798KIE';
+        $micro->modelo='2009';
+        $micro->cant_asiento='20';
+        $micro->num_interno='7898';
+        $micro->foto='https://eju.tv/wp-content/uploads/2020/07/img_5f10e9a46c1cf.jpg';
+        $micro->fecha_asignacion='2022-01-03';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=13; 
+        $micro->id_user=8;
+        $micro->save(); 
+        //Linea 11 VUELTA
+        $micro = new Micro() ;
+        $micro->placa='7798KIE';
+        $micro->modelo='2009';
+        $micro->cant_asiento='20';
+        $micro->num_interno='7889';
+        $micro->foto='https://eju.tv/wp-content/uploads/2020/07/img_5f10e9a46c1cf.jpg';
+        $micro->fecha_asignacion='2022-01-03';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=14; 
+        $micro->id_user=8;
+        $micro->save();
+        //Linea 16 IDA
+        $micro = new Micro() ;
+        $micro->placa='6687LIT';
+        $micro->modelo='2010';
+        $micro->cant_asiento='20';
+        $micro->num_interno='1177';
+        $micro->foto='https://lapublica.org.bo/images/nota_transporte_scz_3.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=15; 
+        $micro->id_user=9;
+        $micro->save(); 
+        //Linea 16 VUELTA
+        $micro = new Micro() ;
+        $micro->placa='6687LIT';
+        $micro->modelo='2010';
+        $micro->cant_asiento='20';
+        $micro->num_interno='1177';
+        $micro->foto='https://lapublica.org.bo/images/nota_transporte_scz_3.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=16; 
+        $micro->id_user=9;
+        $micro->save(); 
+        //Linea 17 IDA
+        $micro = new Micro() ;
+        $micro->placa='6455LOP';
+        $micro->modelo='2011';
+        $micro->cant_asiento='20';
+        $micro->num_interno='7642';
+        $micro->foto='https://eldia.com.bo/images/Noticias/18-8-15/paraGIS.jpg';
         $micro->fecha_asignacion='2022-01-01';
         $micro->fecha_baja='2022-03-01';
         $micro->disponible=1;
         $micro->id_linea=17; 
-        $micro->id_user=4;
+        $micro->id_user=10;
+        $micro->save(); 
+        //Linea 17 VUELTA
+        $micro = new Micro() ;
+        $micro->placa='6455LOP';
+        $micro->modelo='2011';
+        $micro->cant_asiento='20';
+        $micro->num_interno='7642';
+        $micro->foto='https://eldia.com.bo/images/Noticias/18-8-15/paraGIS.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=18; 
+        $micro->id_user=10;
+        $micro->save(); 
+        //Linea 18 IDA
+        $micro = new Micro() ;
+        $micro->placa='7733RIC';
+        $micro->modelo='1999 ';
+        $micro->cant_asiento='20';
+        $micro->num_interno='1189';
+        $micro->foto='http://3.bp.blogspot.com/-jY-m80Pj88E/VMUx7qoGL4I/AAAAAAABy0k/ESVeHjxhvSQ/s1600/Allison%2Brenueva%2Bel%2Btransporte%2Bcon%2Blos%2B%E2%80%98Chuturub%C3%AD%E2%80%992.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=19; 
+        $micro->id_user=11;
+        $micro->save(); 
+        //Linea 18 VUELTA
+        $micro = new Micro() ;
+        $micro->placa='7733RIC';
+        $micro->modelo='1999';
+        $micro->cant_asiento='20';
+        $micro->num_interno='1189';
+        $micro->foto='http://3.bp.blogspot.com/-jY-m80Pj88E/VMUx7qoGL4I/AAAAAAABy0k/ESVeHjxhvSQ/s1600/Allison%2Brenueva%2Bel%2Btransporte%2Bcon%2Blos%2B%E2%80%98Chuturub%C3%AD%E2%80%992.jpg';
+        $micro->fecha_asignacion='2022-01-01';
+        $micro->fecha_baja='2022-03-01';
+        $micro->disponible=1;
+        $micro->id_linea=20; 
+        $micro->id_user=11;
         $micro->save(); 
         //-------------------------------------LINEAS NO DISPONIBLES--------------------------
+        /*
         $micro = new Micro() ;
         $micro->placa='6551ALO';
         $micro->modelo='1995';
@@ -93,5 +316,6 @@ class MicroSeeder extends Seeder
         $micro->id_linea=3; 
         $micro->id_user=4;
         $micro->save(); 
+        */
     }
 }
--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -27,9 +27,11 @@ class UsersSeeder extends Seeder
         $user->cod_chofer=0;
         $user->save();
 
+        //Choferes
+        //Chofer Linea 1
         $user=new User() ;
-        $user->name='Chofer';
-        $user->email='Chofer@gmail.com';
+        $user->name='Miguel Arequipa Smith';
+        $user->email='miguel@gmail.com';
         $user->foto='https://brigadaaltavoz.com/wp-content/uploads/2019/10/71282714_2919252824769951_7677046474420518912_n-1.jpg';
         $user->password=('1234');
         $user->ci=8948057;
@@ -37,18 +39,18 @@ class UsersSeeder extends Seeder
         $user->cod_admin=0;
         $user->cod_chofer=1;
         $user->save();
-
+        //Chofer Linea 2
         $user=new User() ;
         $user->name='Takeshi Kanashiro';
         $user->email='take@gmail.com';
         $user->foto='https://www.supeimage.pngrprof.co/imagenes/anuncios/profesor-home-clases-frances-portugues-para-todas-las-personas-mexicanas-deseen-aprender-esos-bonitos-prodigiosos-idiomas.jpg';
         $user->password=('1234');
-        $user->ci=8948057;
+        $user->ci=11524688;
         $user->fecha_nac='1991-01-17';
         $user->cod_admin=0;
         $user->cod_chofer=1;
         $user->save();
-
+        //Chofer Linea 5
         $user=new User() ;
         $user->name='Oscar Duran';
         $user->email='oscar@gmail.com';
@@ -59,7 +61,83 @@ class UsersSeeder extends Seeder
         $user->cod_admin=0;
         $user->cod_chofer=1;
         $user->save();
-
+        //Chofer Linea 8 NUEVO
+        $user=new User() ;
+        $user->name='Josefa Daniela Mendoza ';
+        $user->email='dani@gmail.com';
+        $user->foto='https://lapatria.bo/wp-content/uploads/2020/11/puyma-katari-1-scaled.jpg';
+        $user->password=('1234'); 
+        $user->ci=67895544;
+        $user->fecha_nac='1979-07-28';
+        $user->cod_admin=0;
+        $user->cod_chofer=1;
+        $user->save();
+        //Chofer Linea 9
+        $user=new User() ;
+        $user->name='Richard Stifler Dent';
+        $user->email='richi@gmail.com';
+        $user->foto='https://diarionuevosur.com/wp-content/uploads/2020/05/chofer.jpg';
+        $user->password=('1234'); 
+        $user->ci=77334486;
+        $user->fecha_nac='1979-07-28';
+        $user->cod_admin=0;
+        $user->cod_chofer=1;
+        $user->save();
+        //Chofer Linea 10
+        $user=new User() ;
+        $user->name='Manuel Lopez Videla';
+        $user->email='manuel@gmail.com';
+        $user->foto='https://ichef.bbci.co.uk/news/640/amz/worldservice/live/assets/images/2010/03/02/100302230013_sp_bolivia_choferes_lider_2.jpg';
+        $user->password=('1234'); 
+        $user->ci=67845288;
+        $user->fecha_nac='1979-07-28';
+        $user->cod_admin=0;
+        $user->cod_chofer=1;
+        $user->save();
+        //Chofer Linea 11
+        $user=new User() ;
+        $user->name='Renato Arteaga';
+        $user->email='renato@gmail.com';
+        $user->foto='https://anteriorportal.erbol.com.bo/sites/default/files/img_noticias/20-jun-deco-6.jpg';
+        $user->password=('1234'); 
+        $user->ci=68978577;
+        $user->fecha_nac='1979-07-28';
+        $user->cod_admin=0;
+        $user->cod_chofer=1;
+        $user->save();
+        //Chofer Linea 16
+        $user=new User() ;
+        $user->name='Juan Manuel Mercado';
+        $user->email='juan@gmail.com';
+        $user->foto='https://www.ccb.com.bo/images/IMG-20220525124156.jpg';
+        $user->password=('1234'); 
+        $user->ci=64551133;
+        $user->fecha_nac='1979-07-28';
+        $user->cod_admin=0;
+        $user->cod_chofer=1;
+        $user->save();
+        //Chofer Linea 17
+        $user=new User() ;
+        $user->name='Ricardo Benavidez';
+        $user->email='ricardo@gmail.com';
+        $user->foto='https://www.somosiberoamerica.org/wp-content/uploads/2019/12/4039ba77f1b97fddf467329e81c2058c-225x225.jpg';
+        $user->password=('1234'); 
+        $user->ci=77663259;
+        $user->fecha_nac='1979-07-28';
+        $user->cod_admin=0;
+        $user->cod_chofer=1;
+        $user->save();
+        //Chofer Linea 18
+        $user=new User() ;
+        $user->name='Andres Andronico Eguez';
+        $user->email='andres@gmail.com';
+        $user->foto='https://anteriorportal.erbol.com.bo/sites/default/files/img_noticias/chofe_chicoteado.jpg';
+        $user->password=('1234'); 
+        $user->ci=78645235;
+        $user->fecha_nac='1979-07-28';
+        $user->cod_admin=0;
+        $user->cod_chofer=1;
+        $user->save();
         //1 2 5 8 9 10 11 16 17 18 TOTAL DE LINEAS
 
 
